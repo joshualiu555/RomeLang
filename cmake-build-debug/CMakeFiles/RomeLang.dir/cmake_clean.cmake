76c47a535fb7821aeca5725b2f2539a88aca0998
joshualiu555 <liu.joshua555@gmail.com>
2024-08-21 09:11:15 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/RomeLang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RomeLang.dir/main.cpp.o"
+  "RomeLang"
+  "RomeLang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RomeLang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
